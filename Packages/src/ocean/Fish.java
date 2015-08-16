a545defece4726658ed468e69a0dbf1b90b1c3be
Adam Carter <adamrcarter@gmail.com>
2015-08-16 14:15:46 -0500

    Packages in Java

---
+++ Packages/src/ocean/Fish.java
@@ -0,0 +1,6 @@
+//File, new class, named it Fish, and in the package field, put ocean
+package ocean;
+
+public class Fish {
+
+}
---
+++ Packages/src/ocean/plants/Algae.java
@@ -0,0 +1,7 @@
+//File, new class, named it Algae, and in the package field, put ocean.plants
+//this puts the plants package inside the ocean package. Like a folder inside a folder
+package ocean.plants;
+
+public class Algae {
+
+}
