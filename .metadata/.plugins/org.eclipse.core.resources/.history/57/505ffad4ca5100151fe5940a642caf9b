7b4cc6073fd5e2c4f388515ea658b3c0ae558a9f
Adam Carter <adamrcarter@gmail.com>
2015-09-02 18:42:06 -0500

    Abstract classes in Java

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/57/505ffad4ca5100151fe5940a642caf9b
@@ -0,0 +1,10 @@
+
+public class Car extends Machine {
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ AbstractClasses/src/Camera.java
@@ -0,0 +1,22 @@
+
+public class Camera extends Machine {
+
+	@Override
+	public void start() {
+		System.out.println("Starting camera");
+		
+	}
+
+	@Override
+	public void doStuff() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void shutdown() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
