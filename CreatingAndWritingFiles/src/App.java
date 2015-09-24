f5b6113b3e6fbd22a5180c09a908e28e2c55831d
Adam Carter <adamrcarter@gmail.com>
2015-09-24 18:52:05 -0500

    Creating and writing to text files with Java

---
+++ CreatingAndWritingFiles/src/App.java
@@ -0,0 +1,21 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+//right click project, refresh, to see created file
+public class App {
+	public static void main(String[] args) {
+		
+		File file = new File("test1.txt");
+
+		try (BufferedWriter br = new BufferedWriter(new FileWriter(file))) {
+			br.write("This is line one");
+			br.write("This is line two");
+			br.newLine();
+			br.write("Last line");
+		} catch (IOException e) {
+			System.out.println("Unable to read file " + file.toString());
+		}
+	}
+}
