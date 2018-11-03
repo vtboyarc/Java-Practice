dd6d09cbb4e5382c64becc7e81d30a50532087f7
vtboyarc <adamrcarter@gmail.com>
2018-11-03 17:23:41 -0500

    Added handlebars template to spark project

--- SparkGradle/src/main/java/Main.java
+++ SparkGradle/src/main/java/Main.java
@@ -1,9 +1,14 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello World wassup");
 
-        get("/", (req, res) -> "Main page, I guess?");
+        get("/", (req, res) -> {
+           return new ModelAndView(null, "index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
