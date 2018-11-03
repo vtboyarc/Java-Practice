acec02e64a0ab273acd05b1e13fdd94c84045c60
vtboyarc <adamrcarter@gmail.com>
2018-11-03 17:52:57 -0500

    Added user login for spark project

--- SparkGradle/src/main/java/Main.java
+++ SparkGradle/src/main/java/Main.java
@@ -1,14 +1,26 @@
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World wassup");
 
         get("/", (req, res) -> {
-           return new ModelAndView(null, "index.hbs");
+            Map<String, String> model = new HashMap<>();
+            model.put("username", req.cookie("username"));
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/sign-in", (req, res) -> {
+            Map<String, String> model = new HashMap<>();
+            String username = req.queryParams("username");
+            res.cookie("username", username);
+            model.put("username", username);
+            return new ModelAndView(model, "sign-in.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
