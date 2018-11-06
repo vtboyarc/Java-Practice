eb0ffb7c1642458f2913abdf5903e9d507f22b7f
vtboyarc <adamrcarter@gmail.com>
2018-11-06 11:01:37 -0600

    Added thymeleafe and style to the spring project

--- SpringBasics/src/main/java/com/adamcarter/giflib/controller/GifController.java
+++ SpringBasics/src/main/java/com/adamcarter/giflib/controller/GifController.java
@@ -2,20 +2,13 @@ package com.adamcarter.giflib.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GifController {
 
     @RequestMapping("/")
-    @ResponseBody
     public String listGifs() {
-    return "List of all the gifs aw yeah!";
+    return "home";
     }
 
-    @RequestMapping("/gif")
-    @ResponseBody
-    public String singleGif() {
-        return "This will be a single gif";
-    }
 }
