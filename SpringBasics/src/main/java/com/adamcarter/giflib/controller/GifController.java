9f28554fcb222a2e90b9bc8e5bbc6bbfe3ab4cdd
vtboyarc <adamrcarter@gmail.com>
2018-11-07 19:01:49 -0600

    Made a POJO, refactored to use it

--- SpringBasics/src/main/java/com/adamcarter/giflib/controller/GifController.java
+++ SpringBasics/src/main/java/com/adamcarter/giflib/controller/GifController.java
@@ -1,14 +1,30 @@
 package com.adamcarter.giflib.controller;
 
+import com.adamcarter.giflib.data.GifRepository;
+import com.adamcarter.giflib.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
 
 @Controller
 public class GifController {
 
+    @Autowired
+    private GifRepository gifRepository;
+
     @RequestMapping("/")
     public String listGifs() {
-    return "home";
+        return "home";
+    }
+
+    @RequestMapping("/gif")
+    public String gifDetails(ModelMap modelMap) {
+        Gif gif = gifRepository.findByName("android-explosion");
+        modelMap.put("gif", gif);
+        return "gif-details";
     }
 
 }
