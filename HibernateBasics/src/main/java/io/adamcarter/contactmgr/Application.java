9d4dda6ffc6a1adf9a8e0e0bab84f1dc09f803e5
vtboyarc <adamrcarter@gmail.com>
2018-11-09 13:24:13 -0600

    Initial setup of Hibernate app

---
+++ HibernateBasics/src/main/java/io/adamcarter/contactmgr/Application.java
@@ -0,0 +1,8 @@
+package io.adamcarter.contactmgr;
+
+public class Application {
+    // in intelliJ, type psvm and it should suggest this auto complete main method for you
+    public static void main(String[] args) {
+
+    }
+}
