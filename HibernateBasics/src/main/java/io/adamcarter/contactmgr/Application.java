7d447e7c910a93fa4fe02c36f3764c65e9c0182e
vtboyarc <adamrcarter@gmail.com>
2018-11-12 09:13:13 -0600

    Added update and delete with Hibernate

--- HibernateBasics/src/main/java/io/adamcarter/contactmgr/Application.java
+++ HibernateBasics/src/main/java/io/adamcarter/contactmgr/Application.java
@@ -2,11 +2,14 @@ package io.adamcarter.contactmgr;
 
 import io.adamcarter.contactmgr.model.Contact;
 import io.adamcarter.contactmgr.model.Contact.ContactBuilder;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.List;
 
 
 public class Application {
@@ -25,6 +28,106 @@ public class Application {
                 .withEmail("me@me")
                 .withPhone(512L)
                 .build();
+        int id = save(contact);
+
+        // Display a list of contacts before the update
+        System.out.printf("%n%nBefore update%n%n");
+        fetchAllContacts().stream().forEach(System.out::println);
+
+        // Get the persisted contact
+        Contact c = findContactById(id);
+
+        // Update the contact
+        c.setFirstName("Wut");
+
+        // Persist the changes
+        System.out.printf("%n%nUpdating...%n%n");
+        update(c);
+        System.out.printf("%n%nUpdate complete%n%n");
+
+        // Display a list of contacts after the update
+        System.out.printf("%n%nAfter update%n%n");
+        fetchAllContacts().stream().forEach(System.out::println);
+
+        // Get the contact with id of 1
+        c = findContactById(2);
+
+        // Delete the contact
+        System.out.printf("%nDeleting...%n");
+        delete(c);
+        System.out.printf("%nDeleted!%n");
+        System.out.printf("%nAfter delete%n");
+        fetchAllContacts().stream().forEach(System.out::println);
+    }
+
+    private static Contact findContactById(int id) {
+        // Open a session
+        Session session = sessionFactory.openSession();
+
+        // Retrieve the persistent object (or null if not found)
+        Contact contact = session.get(Contact.class, id);
+
+        // Close the session
+        session.close();
+
+        // Return the contact object
+        return contact;
+    }
+
+    private static void delete(Contact contact) {
+        // Open a session
+        Session session = sessionFactory.openSession();
+
+        // Begin a transaction
+        session.beginTransaction();
+
+        // Use the session to update the contact
+        session.delete(contact);
+
+        // Commit the transaction
+        session.getTransaction().commit();
+
+        // Close the session
+        session.close();
+    }
+
+    private static void update(Contact contact) {
+        // Open a session
+        Session session = sessionFactory.openSession();
+
+        // Begin a transaction
+        session.beginTransaction();
+
+        // Use the session to update the contact
+        session.update(contact);
+
+        // Commit the transaction
+        session.getTransaction().commit();
+
+        // Close the session
+        session.close();
+
+    }
+
+    @SuppressWarnings("unchecked")
+    private static List<Contact> fetchAllContacts() {
+        // Open a session
+        Session session = sessionFactory.openSession();
+
+        // Create the criteria object
+        Criteria criteria = session.createCriteria(Contact.class);
+
+        // Get a list of contact objects according to the Criteria object created above
+        List<Contact> contacts = criteria.list();
+
+
+        // Close the session
+        session.close();
+
+        return contacts;
+    }
+
+    private static int save(Contact contact) {
         // Open a session
         Session session = sessionFactory.openSession();
 
@@ -33,12 +136,14 @@ public class Application {
 
         // Use the session to save the contact
         // the parameter you pass into this is what you want to save
-        session.save(contact);
+       int id = (int) session.save(contact);
 
         // Commit the transaction
         session.getTransaction().commit();
 
         // Close the session
         session.close();
+
+        return id;
     }
 }
