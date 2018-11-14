a99273129e8846d768bbd4b1480e8c7f15753a5b
vtboyarc <adamrcarter@gmail.com>
2018-11-13 18:45:42 -0600

    Work on categories

--- SpringWithHibernate/src/main/java/io/adamcarter/giflib/web/controller/CategoryController.java
+++ SpringWithHibernate/src/main/java/io/adamcarter/giflib/web/controller/CategoryController.java
@@ -3,11 +3,13 @@ package io.adamcarter.giflib.web.controller;
 
 import io.adamcarter.giflib.model.Category;
 import io.adamcarter.giflib.service.CategoryService;
+import io.adamcarter.giflib.web.Color;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
@@ -27,6 +29,16 @@ public class CategoryController {
         return "category/index";
     }
 
+    // Form for adding a new category
+    @RequestMapping("categories/add")
+    public String formNewCategory(Model model) {
+        model.addAttribute("category", new Category());
+        model.addAttribute("colors", Color.values());
+
+        return "category/form";
+    }
+
+
     // Single category page
     @RequestMapping("/categories/{categoryId}")
     public String category(@PathVariable Long categoryId, Model model) {
@@ -35,5 +47,20 @@ public class CategoryController {
         model.addAttribute("category",category);
 
         return "category/details";
+    }
+
+    // Add a category
+    @RequestMapping(value = "/categories", method = RequestMethod.POST)
+    public String addCategory(Category category) {
+        categoryService.save(category);
+
+        return "redirect:/categories";
+    }
+
+    // Delete an existing category
+    @RequestMapping(value = "/categories/{categoryId}/delete", method = RequestMethod.DELETE)
+    public String deleteCategory(@PathVariable Long categoryId) {
+
+        return null;
     }
 }
