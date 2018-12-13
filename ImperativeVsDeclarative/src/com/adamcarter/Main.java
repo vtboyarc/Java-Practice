f488330dd70d2eacaa285505189f8433297e1b7e
vtboyarc <adamrcarter@gmail.com>
2018-12-12 18:22:26 -0600

    functional programming intro

---
+++ ImperativeVsDeclarative/src/com/adamcarter/Main.java
@@ -0,0 +1,37 @@
+package com.adamcarter;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> ingredients = Arrays.asList(
+                "flour",
+                "salt",
+                "baking powder",
+                "butter",
+                "eggs",
+                "milk"
+        );
+
+        // this is declarative programming - which is used in functional programming
+        // it is more descriptive, verbose
+//        boolean hasEggs = false;
+//        for (String ingredient : ingredients) {
+//            if (ingredient.equals("eggs")) {
+//                hasEggs = true;
+//                break;
+//            }
+//        }
+
+//        if (hasEggs) {
+//            System.out.println("Sorry, it has eggs");
+//        }
+
+        // imperative programming, we can skip the above code and get same results, like so:
+        if (ingredients.contains("eggs")) {
+            System.out.println("Sorry, it has eggs");
+        }
+    }
+}
